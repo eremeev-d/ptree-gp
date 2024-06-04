fd9a4227da44eca60ac223a01b979596f27e335e
erermeev-d <eremeevd2002@yandex.ru>
2024-06-04 16:13:08 +0300

    move requirements to setup.py

--- src/scripts/benchmark.py
+++ src/scripts/benchmark.py
@@ -4,7 +4,6 @@ import json
 import os
 
 import numpy as np
-import pandas as pd
 
 from ptree_gp.kernels import (
     PTreeHeatPrecomputedKernel, PTreeMaternPrecomputedKernel,
--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,22 @@
 import setuptools
 
+requirements = (
+    "numpy>=1.23.5"
+)
+
+extra_requirements = {
+    "tests": (
+        "pytest>=7.4.4"
+    )
+}
+
 setuptools.setup(
     name="ptree_gp",
     version="0.0.1",
     author="Eremeev Dmitrii",
     author_email="eremeev.dima.2002@mail.ru",
-    description="Gaussian Processes on phylogenetic trees",
+    description="Gaussian processes on phylogenetic trees",
     packages=["ptree_gp"],
+    install_requires=requirements,
+    extras_require=extra_requirements
 )
